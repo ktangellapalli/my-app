bae514ab0b63022bb5b5136b45ef1f12241d5a3e
Kiran Tangellapalli <ktangellapalli@evoketechnologies.com>
2018-05-03 13:37:45 +0530

    Conditonal Rendering

--- src/App.js
+++ src/App.js
@@ -3,36 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 import Welcome from './welcome';
 
-
+class LoginText extends Component {
+  render(){
+    return <h2>Please Login</h2>
+  }
+}
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       showToggle: true,
-      welcomeName: "KIRAN"
+      welcomeName: "KIRAN",
+      isLogged: false
     };
     this.handleToggle = this.handleToggle.bind(this);
   }
   handleToggle(){
-    this.setState( () => ({
-      welcomeName: "Ramana"
+    this.setState( prevState => ({
+      isLogged: !prevState.isLogged
     }));
   }
   render() {
+    let loggedIn = null;
+    if(this.state.isLogged){
+      loggedIn = <Welcome greetingName={this.state.welcomeName}/>
+    } else {
+      loggedIn = <LoginText />
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome greetingName={this.state.welcomeName}/>
+          {loggedIn}          
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={this.handleToggle}>Change Greeting</button>
+        <button onClick={this.handleToggle}>{this.state.isLogged ? "Logout" : "Login"}</button>
       </div>
     );
   }
+
 }
 
 export default App;
