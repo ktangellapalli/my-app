e4d7add15de24c4f3b538204842241fc91d1f95c
Kiran Tangellapalli <ktangellapalli@evoketechnologies.com>
2018-05-09 12:52:56 +0530

    Table changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Welcome from './welcome';
+import axios from 'axios';
 
 class LoginText extends Component {
   render(){
     return <h2>Please Login</h2>
+  }
+}
+
+class UsersLists extends Component {
+  constructor(props){
+    super(props)
+    console.log(this.props)
+  }
+
+  render(){
+    var self = this;
+      return (<tbody>
+        { Object.keys(self.props.users).map((key) => (
+        <tr key={ self.props.users[key].id }>
+          <td>{self.props.users[key].first_name}</td>
+          <td>{self.props.users[key].last_name}</td>
+          <td>{self.props.users[key].email}</td>
+          <td>{self.props.users[key].gender}</td>
+        </tr>
+        ))
+        }
+        </tbody>);
   }
 }
 
@@ -15,9 +38,17 @@ class App extends Component {
     this.state = {
       showToggle: true,
       welcomeName: "KIRAN",
-      isLogged: false
+      isLogged: false,
+      users: {}
     };
     this.handleToggle = this.handleToggle.bind(this);
+  }
+  componentDidMount(){
+    axios.get('/users.json').then(response => {
+      this.setState({
+        users: response.data
+      })
+    })
   }
   handleToggle(){
     this.setState( prevState => ({
@@ -31,16 +62,30 @@ class App extends Component {
     } else {
       loggedIn = <LoginText />
     }
+    let users = null;
+    if(this.state.users.length > 0) {
+      users = <UsersLists users={this.state.users} />;
+    } else {
+      users = "No users found";
+    }
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {loggedIn}          
+          {loggedIn}  
+          <button onClick={this.handleToggle}>{this.state.isLogged ? "Logout" : "Login"}</button>        
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={this.handleToggle}>{this.state.isLogged ? "Logout" : "Login"}</button>
+        <table className="table-center">
+            <thead>
+              <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Gender</th>
+              </tr>
+            </thead>
+            {users}
+        </table>
       </div>
     );
   }
